9b770ae3a3ee1912472596bb7aef689f95de8220
polosa00 <i@apolochanin.ru>
2023-06-03 17:28:07 +0300

    Initial Commit

---
+++ FirebaseAuthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseAuthApp
+//
+//  Created by Александр Полочанин on 3.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
